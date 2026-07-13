7aa10ca6c34de98cfcf32ea1d04323bc678601b0
jeloseki <jeloseki@gmail.com>
2026-07-13 18:54:22 +0200

    vjezba JS

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,22 @@
+// Ovo je komentar
+alert('Hello World 1') // ; se može i ne mora staviti
+
+/*
+Ovo je komentar kroz više linija
+
+*/
+
+document.getElementById('ispis').innerText='Hello World 6'
+document.getElementById('ispis').style.color='red'
+
+document.getElementById('ge').innerHTML='<a href="#">Ovo je poveznica<a/>'
+
+// Backtics
+console.log(`
+    
+    Ovaj tekst mogu
+    pisati kroz više 
+    redova....
+    kasnije druge pogodnosti
+    
+    `)
