07e3e44c68ec764d176b7d6291d6afdc2ee3ce0a
jeloseki <jeloseki@gmail.com>
2026-09-23 18:48:45 +0200

    CRUD radili Read 1/4 i podaci

--- 04_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 04_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
 
 
 export default function SmjerPregled(){
+
+   const [smjerovi, setSmjerovi]  = useState([])
+
+   useEffect(()=>{
+       console.log('Dosao na pregled smjerova')
+       ucitajSmjerove()
+   },[])
+
+   async function ucitajSmjerove(){
+    await SmjerService.get().then((odgovor)=>{
+        //console.table(odgovor.data)
+        setSmjerovi(odgovor.data)
+    })
+   }
+
     return(
         <>
         Ovdje dođe pregled smjerova
+        <hr />
+        {JSON.stringify(smjerovi,null,2)}
         </>
     )
 }
---
+++ 04_REACT/EdunovaAPP/src/services/smjerovi/SmjerPodaci.js
@@ -0,0 +1,21 @@
+export const smjerovi = [
+    {
+        sifra: 1, 
+        naziv: 'Frontend developer', 
+        trajanje: 350, 
+        cijena: 2199.99, 
+        datumPokretanja: '2024-02-21T17:00:00', 
+        aktivan: true
+    }, 
+    {
+        sifra: 2, 
+        naziv: 'Knjigovodstvo', 
+        trajanje: 200, 
+        cijena: 1500, 
+        datumPokretanja: '2019.09-04T19:30:00',
+        aktivan: false
+    }, 
+    {
+        
+    }
+]
